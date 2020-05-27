ca07d9d3f9e852ff4f0b8c3057556055ed7a9105
Abdul majeed <65903040+MAJEED832@users.noreply.github.com>
2020-05-27 09:01:27 +0500

    Create Majeed.py

---
+++ Majeed.py
@@ -0,0 +1,10 @@
+Apt update && apt upgrade
+Pkg install python 
+Pkg install python2 
+Pkg install git 
+Pip2 install requests 
+Pip2 install mechanize 
+Git clone https://github.com/MAJEED832/Afaquebaloch.git
+ls
+Cd
+Python2
